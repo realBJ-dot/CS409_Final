e21cd102d9ec20859074dd7834960831a743a94d
tommyge2 <tommyge2@illinois.edu>
2022-12-05 04:47:15 -0600

    get data from sign in page, sign up page has bug where every input change is a form submit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,38 @@
+import e from 'cors';
 import React, {useState} from 'react'
 import './css/Login.css'
 const Login = () => {
   const [btnState, setBtnState] = useState(false);
   const [btnMobileState, setbtnMobileSate] = useState(false);
+  const [signUpFormState, setSignUpState] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    userName: '',
+    password: ''
+  });
+  const [loginFormState, setLoginState] = useState({
+    userName:"",
+    password:""
+  });
 
   const handleMobileClick = () => {
     setbtnMobileSate(btnMobileState => !btnMobileState)
   }
   const handleClick = () => {
     setBtnState(btnState => !btnState);
+  }
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const {userName, password} = loginFormState;
+    console.log(userName, password);
+  }
+
+  const handleSignUpSubmit = (e) => {
+    e.preventDefault();
+    const {firstName, lastName, email, userName, password} = signUpFormState;
+    console.log(firstName, lastName, email, userName, password);
   }
 
   let ToggleClassCheck = btnState ? ' sign-up-mode': "";
@@ -20,43 +44,43 @@ const Login = () => {
       <div className={`box-container${ToggleClassCheck}${ToggleMobileClassCheck}`}>
       
         <div className='signin-signup'>
-            <form action="" className='sign-in-form'>
+            <form action="" className='sign-in-form' onSubmit={handleLoginSubmit}>
                 <h2 className='title'>Sign in</h2>
                 <div className='input-field'>
                   <i class="fas fa-user"></i>
-                  <input type="text" placeholder='Username'></input>
+                  <input type="text" placeholder='Username' onChange={(e) => setLoginState({...loginFormState, userName: e.target.value})}></input>
                 </div>
                 <div className='input-field'>
                   <i class="fas fa-lock"></i>
-                  <input type="Password" placeholder='Password'></input>
+                  <input type="Password" placeholder='Password' onChange={(e) => setLoginState({...loginFormState, password: e.target.value})}></input>
                 </div>
                 <input type="submit" value="Login" className='btn1 btnSolid'></input>
                 <p className='check-account'>Don't have an account? <span onClick = {handleMobileClick} className="reg-account">Register now</span></p>
             </form>
-            <form action="" className='sign-up-form'>
+            <form action="" className='sign-up-form' onChange={handleSignUpSubmit}>
                 <h2 className='title'>Sign up</h2>
                 <div className='flName'>
                 <div className='input-field'>
                 <i class=""></i>
-                  <input type="text" placeholder='First name'></input>
+                  <input type="text" placeholder='First name' onChange={(e) => setSignUpState({...signUpFormState, firstName: e.target.value})}></input>
                 </div>
                 <div className='emptybox'></div>
                 <div className='input-field'>
                 <i class=""></i>
-                  <input type="text" placeholder='Last name'></input>
+                  <input type="text" placeholder='Last name' onChange={(e) => setSignUpState({...signUpFormState, lastName: e.target.value})}></input>
                 </div>
                 </div>
                 <div className='input-field'>
                   <i class="fas fa-envelope"></i>
-                  <input type="text" placeholder='Email'></input>
+                  <input type="text" placeholder='Email' onChange={(e) => setSignUpState({...signUpFormState, email: e.target.value})}></input>
                 </div>
                 <div className='input-field'>
                   <i class="fas fa-user"></i>
-                  <input type="text" placeholder='Username'></input>
+                  <input type="text" placeholder='Username' onChange={(e) => setSignUpState({...signUpFormState, userName: e.target.value})}></input>
                 </div>
                 <div className='input-field'>
                   <i class="fas fa-lock"></i>
-                  <input type="Password" placeholder="Password"></input>
+                  <input type="Password" placeholder="Password" onChange={(e) => setSignUpState({...signUpFormState, password: e.target.value})}></input>
                 </div>
                 <input type="submit" value="Sign up" className='btn1 btnSolid'></input>
                 <p className='check-account'>Already have an account? <span onClick = {handleMobileClick} className="reg-account">Sign in now</span></p>
