4a2aa90eaf3fad70e9fdff475440504ffcb99681
Xinshuo Lei <leixshuo@gmail.com>
2022-12-05 21:05:01 -0600

    add endpoint for retrieving and adding transactions

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const createAccount = require('./routes/signupandlogin')
+const createAccount = require('./routes/authentication')
 const loadUser = require('./routes/users')
+const transaction = require('./routes/transactions')
 
-router.use("/api",createAccount,loadUser);
+router.use("/api", createAccount, loadUser, transaction);
 
 module.exports = router;
