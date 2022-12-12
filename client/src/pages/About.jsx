89feb18a665e54cf3118ba8390a26dc466bc10b3
tommyge2 <tommyge2@illinois.edu>
2022-12-11 23:44:47 -0600

    fix about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,30 +7,30 @@ const about = () => {
       <h1>
         Expense Tracker is an app started by 4 students in CS 409 at UIUC. Read more about us here or check out our app!
       </h1>
-      <div class='box'>
-        <div class='row'>
-          <div class='column'>
-            <div class='col1'>
+      <div className='box'>
+        <div className='row'>
+          <div className='column'>
+            <div className='col1'>
               <h1>Chien Nguyen</h1>
               <h3>Frontend Developer</h3>
             </div>
           </div>
-          <div class='column'>
-            <div class='col2'>
+          <div className='column'>
+            <div className='col2'>
               <h1>Peiyuan Jin</h1>
               <h3>Frontend Developer</h3>            
             </div>
           </div>
         </div>
-        <div class='row 2'>
-          <div class='column'>
-            <div class='col2'>
+        <div className='row 2'>
+          <div className='column'>
+            <div className='col2'>
             <h1>Xinshuo Lei</h1>
             <h3>Backend Developer</h3>            
           </div>
           </div>
-          <div class='column'>
-            <div class='col1'>
+          <div className='column'>
+            <div className='col1'>
             <h1>Tommy Ge</h1>
             <h3>Backend Developer</h3>            
           </div>
