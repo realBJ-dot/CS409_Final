305afc0f98933c6b71da9bf957716bccb00ff9a3
Chien Nguyen <cnguyen1494@gmail.com>
2022-12-12 00:04:44 -0600

    updated about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,43 +1,45 @@
-import React from 'react'
-import './css/About.css'
+import React from "react";
+import "./css/About.css";
 
 const about = () => {
   return (
-    <div className='aboutdiv'>
+    <div className="aboutdiv">
       <h1>
-        Expense Tracker is an app started by 4 students in CS 409 at UIUC. Read more about us here or check out our app!
+        Expense Tracker is an app started by 4 students in CS 409 at UIUC. Read
+        more about us here or check out our app!
       </h1>
-      <div className='box'>
-        <div className='row'>
-          <div className='column'>
-            <div className='col1'>
+      <div className="box">
+        <div className="row">
+          <div className="column">
+            <div className="col1">
               <h1>Chien Nguyen</h1>
               <h3>Frontend Developer</h3>
             </div>
           </div>
-          <div className='column'>
-            <div className='col2'>
+          <div className="column">
+            <div className="col2">
               <h1>Peiyuan Jin</h1>
-              <h3>Frontend Developer</h3>            
+              <h3>Frontend Developer</h3>
             </div>
           </div>
         </div>
-        <div className='row 2'>
-          <div className='column'>
-            <div className='col2'>
-            <h1>Xinshuo Lei</h1>
-            <h3>Backend Developer</h3>            
+        <div className="row 2">
+          <div className="column">
+            <div className="col2">
+              <h1>Xinshuo Lei</h1>
+              <h3>Backend Developer</h3>
+            </div>
           </div>
-          </div>
-          <div className='column'>
-            <div className='col1'>
-            <h1>Tommy Ge</h1>
-            <h3>Backend Developer</h3>            
-          </div>
+          <div className="column">
+            <div className="col1">
+              <h1>Tommy Ge</h1>
+              <h3>Backend Developer</h3>
+            </div>
           </div>
         </div>
-</div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default about
+export default about;
